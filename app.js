c61220c67e92b5bd0f4e0ac8e17bf06ebb910dc8
yohanesnaibaho303 <yohanespratama303@gmail.com>
2023-02-27 18:57:31 +0700

    add: initial commit

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require("express");
+const cors = require('cors');
+const ipModel = require("./ip.model");
+
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cors());
 app.get("/", (req, res) => res.type('html').send(html));
+app.get("/location", (req, res) => res.json({ location: "cikarang" }));
+
+app.get("/visitor", async (req, res) => {
+  let ipData = new ipModel({
+   ip_addr: req.ip,
+   date_added: new Date(),
+  });
+  await ipData.save();
+
+  let numVisits = await ipModel.count()
+
+  return res.json({ your_ip: req.ip, visitor_number: numVisits });
+ });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
 
 const html = `
 <!DOCTYPE html>
